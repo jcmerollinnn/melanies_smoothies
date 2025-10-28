99161263e77731d9d4ccc7d1f0a90ee9ef0746f9
JC <127466020+jcmerollinnn@users.noreply.github.com>
2025-10-28 12:59:16 +0800

    Update streamlit_app.py
    imported REST API calls and added fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
    
         st.success('Your Smoothie is ordered! ' + name_on_order, icon="✅")
+
+#New section to display FruityVice  nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/melon"
+st.text(fruityvice_response)
