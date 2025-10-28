37de2860dfb0b49c2f5c2168ffc480027447279a
JC <127466020+jcmerollinnn@users.noreply.github.com>
2025-10-28 13:02:13 +0800

    Update streamlit_app.py
    fixed line 48, no parentheses at the end

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,6 @@ if ingredients_list:
 
 #New section to display FruityVice  nutrition information
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/melon"
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/melon")
 st.text(fruityvice_response.json())
                                    
