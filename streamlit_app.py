ff72266eb75717ec898913e295b9bad1fda12b19
JC <127466020+jcmerollinnn@users.noreply.github.com>
2025-10-28 13:06:53 +0800

    Update streamlit_app.py
    Place the JSON into a dataframe using sf_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,6 @@ if ingredients_list:
 #New section to display FruityVice  nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/melon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
                                    
